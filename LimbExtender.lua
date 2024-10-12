a2f4b164b830a2791914134813e961ab0755cc34
AAPV15 <146614549+AAPV15@users.noreply.github.com>
2024-10-12 23:32:02 +0800

    Update LimbExtender.lua

--- LimbExtender.lua
+++ LimbExtender.lua
@@ -230,6 +230,7 @@ local function startProcess()
             onPlayerCharacterAdded(player)
         else
             player.CharacterAdded:Connect(function(character)
+                character:WaitForChildWhichIsA("Humanoid")
                 LimbsFolder.Parent = character
             end)
 
