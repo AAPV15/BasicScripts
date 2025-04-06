a4ecfe26052861809072a225dab1f5cd995b70d6
AAPV15 <146614549+AAPV15@users.noreply.github.com>
2025-04-06 22:24:51 +0800

    Update LimbExtender.lua

--- LimbExtender.lua
+++ LimbExtender.lua
@@ -1,2 +1,2 @@
-warn("This loadstring is outdated.")
+warn("This loadstring is outdated. You will be rediracted to the new version")
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AAPVdev/scripts/refs/heads/main/UI_LimbExtender.lua'))()
